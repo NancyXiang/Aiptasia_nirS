f43be5809cd231ff3dd26bad490cc3ac28e6d552
Nan Xiang <95756687+NancyXiang@users.noreply.github.com>
2022-04-27 21:19:17 +0200

    Add files via upload

---
+++ Taxonomic Assignment/nirS_Prot_database_prep.R
@@ -0,0 +1,31 @@
+### ### ### ### ### ### ### ### ###
+###   nirS  prot database prep  ###
+### ### ### ### ### ### ### ### ###
+
+## after getting nirS database training fasta
+
+## prepare nirS database excluding unculture organsims for taxonomic assignment
+
+setwd("/Users/nanxiang/Desktop/PhD/WP3_Aiptasia_nirS/nirS_analysis")
+save.image("database_nirS_excl_uncul.Rdata")
+
+library(seqinr)
+library(dplyr)
+
+fasta<-as.data.frame(phylotools::read.fasta("/Users/nanxiang/Desktop/PhD/WP3_Aiptasia_nirS/nirS_analysis/database/fungene_repository_nirS_prot_ref_25330.fasta"))
+
+fasta$species<-gsub(".*organism=", "", fasta$seq.name)
+
+fasta$genus<-sub(" .*", "", fasta$species)
+
+fasta$genus= as.factor(fasta$genus)
+levels(fasta$genus)
+
+# subset by removing genus = "uncultured"
+fasta_subset=subset(fasta, fasta$genus != "uncultured")
+
+# check specie numbers
+fasta_subset$species_tax=gsub(",.*", "", fasta_subset$species)
+species_final=fasta_subset %>% group_by(species_tax) %>%  sample_n(1)
+
+write.fasta(as.list(fasta_subset$seq.text), fasta_subset$seq.name, "/Users/nanxiang/Desktop/PhD/WP3_Aiptasia_nirS/nirS_analysis/database/nirS_prot_dp_excludingUnc_3504.fasta", open = "w", nbchar = 60, as.string = FALSE)
